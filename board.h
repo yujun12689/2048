f088ca27aa5bb974cfb2e71fdf1ceae395a3621b
yujun12689 <yujun12689@gmail.com>
2021-10-10 16:33:11 +0800

    merge and score adding

--- board.h
+++ board.h
@@ -101,9 +101,10 @@ public:
 				if (tile == 0) continue;
 				row[c] = 0;
 				if (hold) {
-					if (tile == hold) {
+					if (std::abs(tile - hold)==1 || (tile == 1 && hold == 1)) {
+						tile = std::max(tile, hold) + 1;
 						row[top++] = ++tile;
-						score += (1 << tile);
+						score += fib(tile);
 						hold = 0;
 					} else {
 						row[top++] = hold;
